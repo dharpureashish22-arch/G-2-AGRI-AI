b41d4ee3bed4adf7d3b01fa03ec1736d1956a3f6
dharpureashish22-arch <dharpureashish22@gmail.com>
2026-06-03 07:34:10 +0530

    Rename model to 'gemini-1.5 flash'

--- G-2.py
+++ G-2.py
@@ -16,7 +16,7 @@ except Exception:
 # --- 2. AI Model setup (Web Browser ke sath) ---
 # Yahan 'tools="google_search_retrieval"' wali line se AI ko internet ka access mil gaya
 model = genai.GenerativeModel(
-    model_name="gemini-3.1-flash lite",
+    model_name="gemini-1.5 flash",
     tools="google_search_retrieval" 
 )
 
